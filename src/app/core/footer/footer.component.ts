72f042f88393d15954d3d3e99f5001c0de79a0b7
Quentin Heroguel <q_hero@outlook.com>
2024-01-06 14:48:02 +0100

    style: added footer style

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { DatePipe } from "@angular/common";
-import { RouterLink } from "@angular/router";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: "app-footer",
-  templateUrl: "./footer.component.html",
+  selector: 'app-footer',
+  styleUrl: './footer.component.css',
+  templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [DatePipe, RouterLink],
   standalone: true,
