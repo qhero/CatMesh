dd4ed1225aed01f005cb72b4f6c72ca166c62bc1
qhero <q_hero@outlook.com>
2024-01-06 15:26:10 +0100

    Style/styling layout (#1)
    * packages: update angular dev-kit
    
    * package: updated angular cli
    
    * packages: fixing packages issue, add material ui
    
    * style: added footer style
    
    * style: setting up style for header and match the footer  bg color

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,14 +1,12 @@
-import { Component } from "@angular/core";
-import { RouterLink, RouterLinkActive } from "@angular/router";
-
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  imports: [
-    RouterLinkActive,
-    RouterLink,
-  ],
-  standalone:true
+  selector: 'app-header',
+  styleUrl: './header.component.css',
+  templateUrl: './header.component.html',
+  imports: [RouterLinkActive, RouterLink, MatSidenavModule],
+  standalone: true,
 })
 export class HeaderComponent {}
